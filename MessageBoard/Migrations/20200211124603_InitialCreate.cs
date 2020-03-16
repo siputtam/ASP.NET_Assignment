f8e18f0a7647b4d86d197bd4b0d62ebca18811de
UTTAM KUMAR DAS <sipu.uttam@gmail.com>
2020-03-16 09:48:20 +0530

    first commit

---
+++ MessageBoard/Migrations/20200211124603_InitialCreate.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MessageBoard.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(250)", nullable: true),
+                    Message = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    Likes = table.Column<string>(type: "nvarchar(500)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ MessageBoard/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MessageBoard.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+        [Column(TypeName ="nvarchar(250)")]
+        public string Name { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string Message { get; set; }
+        [Column(TypeName = "nvarchar(500)")]
+        public int Likes { get; set; }
+
+    }
+}
